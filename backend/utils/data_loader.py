8b31f33ef729cf47cf2eb7d5d6b7f02766315f0b
Napatchol Thaipanich <napatchol.tha@gmail.com>
2025-07-23 18:06:39 +0100

    feat: call etherscan as many as possible

--- backend/utils/data_loader.py
+++ backend/utils/data_loader.py
@@ -1,17 +1,38 @@
 import os
 import json
+import time
 import requests
+from dotenv import load_dotenv
 
-def get_transactions_by_contract_addr(api_key, addr):
-    url = "https://api.etherscan.io/api"
+load_dotenv()
+
+ETHERSCAN_API_KEY = os.getenv("ETHERSCAN_API_KEY")
+
+url = "https://api.etherscan.io/v2/api"
+
+def get_most_recent_blocknumber(chainid=1):
     params = {
-        "module": "account",
-        "action": "txlist",
+        "chainid": chainid,  # Ethereum mainnet = 1
+        "module": "proxy",
+        "action": "eth_blockNumber",
+        "apikey": ETHERSCAN_API_KEY
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        data = response.json()
+        return int(data["result"], 16)
+    except Exception as e:
+        print(f"error: {e}")
+        return 0
+
+def get_events_by_contract_addr(addr, chainid=1):
+    params = {
+        "chainid": chainid,  # Ethereum mainnet = 1
+        "module": "logs",
+        "action": "getLogs",
+        "apikey": ETHERSCAN_API_KEY,
         "address": addr,
-        "startblock": 0,
-        "endblock": 99999999,
-        "sort": "asc",
-        "apikey": api_key
     }
 
     try:
@@ -20,55 +41,193 @@ def get_transactions_by_contract_addr(api_key, addr):
         if data["status"] == "1":
             return data["result"]
         else:
-            print(f"❌ No transactions found or error: {data.get('message', '')}")
+            print(f"error: {data.get('message', '')}")
             return []
     except Exception as e:
-        print(f"🚨 Error fetching transactions: {e}")
+        print(f"error: {e}")
         return []
 
+def get_normal_transactions_by_contract_addr(addr, chainid=1):
+    params = {
+        "chainid": chainid,  # Ethereum mainnet = 1
+        "module": "account",
+        "action": "txlist",
+        "address": addr,
+        "startblock": 0,
+        "endblock": get_most_recent_blocknumber(chainid),
+        "sort": "asc",
+        "apikey": ETHERSCAN_API_KEY
+    }
 
+    try:
+        response = requests.get(url, params=params)
+        data = response.json()
+        if data["status"] == "1":
+            return data["result"]
+        else:
+            print(f"error: {data.get('message', '')}")
+            return []
+    except Exception as e:
+        print(f"error: {e}")
+        return []
 
-def save_transactions_by_contract_addr(save_folder, addr, txns):
+def get_internal_transactions_by_contract_addr(addr, chainid=1):
+    params = {
+        "chainid": chainid,  # Ethereum mainnet = 1
+        "module": "account",
+        "action": "txlistinternal",
+        "address": addr,
+        "startblock": 0,
+        "endblock": get_most_recent_blocknumber(chainid),
+        "sort": "asc",
+        "apikey": ETHERSCAN_API_KEY
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        data = response.json()
+        if data["status"] == "1":
+            return data["result"]
+        else:
+            print(f"error: {data.get('message', '')}")
+            return []
+    except Exception as e:
+        print(f"error: {e}")
+        return []
+
+def get_contract_creator_by_contract_addr(addr, chainid=1):
+    params = {
+        "chainid": chainid,  # Ethereum mainnet = 1
+        "module": "contract",
+        "action": "getcontractcreation",
+        "contractaddresses": addr,
+        "apikey": ETHERSCAN_API_KEY
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        data = response.json()
+        if data["status"] == "1":
+            return data["result"][0]
+        else:
+            print(f"error: {data.get('message', '')}")
+            return dict()
+    except Exception as e:
+        print(f"error: {e}")
+        return dict()
+
+def get_balance_by_contract_addr(addr, chainid=1):
+    params = {
+        "chainid": chainid,  # Ethereum mainnet = 1
+        "module": "account",
+        "action": "balance",
+        "address": addr,
+        "tag": "latest",
+        "apikey": ETHERSCAN_API_KEY
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        data = response.json()
+        if data["status"] == "1":
+            return int(data["result"])
+        else:
+            print(f"error: {data.get('message', '')}")
+            return 0
+    except Exception as e:
+        print(f"error: {e}")
+        return 0
+
+def get_token_supply_by_contract_addr(addr, chainid=1):
+    params = {
+        "chainid": chainid,  # Ethereum mainnet = 1
+        "module": "stats",
+        "action": "tokensupply",
+        "contractaddress": addr,
+        "apikey": ETHERSCAN_API_KEY
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        data = response.json()
+        if data["status"] == "1":
+            return int(data["result"])
+        else:
+            print(f"error: {data.get('message', '')}")
+            return 0
+    except Exception as e:
+        print(f"error: {e}")
+        return 0
+
+def get_transaction_count_by_contract_addr(addr, chainid=1):
+    params = {
+        "chainid": chainid,  # Ethereum mainnet = 1
+        "module": "proxy",
+        "action": "eth_getTransactionCount",
+        "address": addr,
+        "tag": "latest",
+        "apikey": ETHERSCAN_API_KEY
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        data = response.json()
+        if "result" in data:
+            return int(data["result"], 16)
+
+        return 0
+    except Exception as e:
+        print(f"error: {e}")
+        return 0
+
+def get_transaction_count_by_contract_addr(addr, chainid=1):
+    params = {
+        "chainid": chainid,  # Ethereum mainnet = 1
+        "module": "proxy",
+        "action": "eth_getTransactionCount",
+        "address": addr,
+        "tag": "latest",
+        "apikey": ETHERSCAN_API_KEY
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        data = response.json()
+        if data["status"] == "1":
+            return int(data["result"])
+        else:
+            print(f"error: {data.get('message', '')}")
+            return 0
+    except Exception as e:
+        print(f"error: {e}")
+        return 0
+
+def get_info_by_contract_addr(addr, chainid=1):
+    info = dict()
+    info['creator'] = get_contract_creator_by_contract_addr(addr, chainid)
+    info['balance'] = get_balance_by_contract_addr(addr, chainid)
+    info['transaction'] = get_normal_transactions_by_contract_addr(addr, chainid) + get_internal_transactions_by_contract_addr(addr, chainid)
+    info['event'] = get_events_by_contract_addr(addr, chainid)
+    time.sleep(1)
+    return info
+
+def save_transactions_by_contract_addr(save_folder, addr, info):
     os.makedirs(save_folder, exist_ok=True)
     filename = f"{addr.lower()}.json"
     file_path = os.path.join(save_folder, filename)
-
     with open(file_path, "w") as f:
-        json.dump(txns, f, indent=2)
+        json.dump(info, f, indent=4)
 
     return file_path
 
-# def get_bytecode_by_contract_addr(api_key, addr):
-#     url = f"https://eth-mainnet.g.alchemy.com/v2/{api_key}"
-#     payload = {
-#         "jsonrpc": "2.0",
-#         "id": 1,
-#         "method": "eth_getCode",
-#         "params": [addr, "latest"]
-#     }
-#     headers = {"Content-Type": "application/json"}
-
-#     try:
-#         response = requests.post(url, json=payload, headers=headers)
-#         data = response.json()
-#         if "result" in data:
-#             return data["result"]
-#         else:
-#             print(f"❌ Error from Alchemy: {data.get('error', {}).get('message', 'Unknown error')}")
-#             return ""
-#     except Exception as e:
-#         print(f"🚨 Exception fetching bytecode: {e}")
-#         return ""
-
-def get_bytecode_by_contract_addr(api_key, addr):
-    url = "https://api.etherscan.io/v2/api"
+def get_bytecode_by_contract_addr(addr, chainid=1):
     params = {
-        "chainid": 1,  # Ethereum mainnet
+        "chainid": chainid,  # Ethereum mainnet = 1
         "module": "proxy",
         "action": "eth_getCode",
         "address": addr,
         "tag": "latest",
-        "apikey": api_key
+        "apikey": ETHERSCAN_API_KEY
     }
 
     try:
@@ -77,12 +236,11 @@ def get_bytecode_by_contract_addr(api_key, addr):
         if "result" in data:
             return data["result"]
         else:
-            print(f"❌ Etherscan Error: {data.get('message', 'Unknown error')}")
+            print(f"error: {data.get('message', 'Unknown error')}")
             return ""
     except Exception as e:
-        print(f"🚨 Exception fetching bytecode from Etherscan: {e}")
+        print(f"error: {e}")
         return ""
-
 
 def save_bytecode_by_contract_addr(save_folder, addr, bytecode):
     os.makedirs(save_folder, exist_ok=True)
@@ -93,3 +251,24 @@ def save_bytecode_by_contract_addr(save_folder, addr, bytecode):
         f.write(bytecode)
 
     return file_path
+
+def reached_limit():
+    params = {
+        "module": "getapilimit",
+        "action": "getapilimit",
+        "apikey": ETHERSCAN_API_KEY
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        data = response.json()
+        if data["status"] == "1":
+            return data["result"]
+        else:
+            print(f"error: {data.get('message', '')}")
+            if "result" in data:
+                return data["result"]
+            return dict()
+    except Exception as e:
+        print(f"error: {e}")
+        return dict()

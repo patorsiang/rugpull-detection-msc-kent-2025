e6609cbe58ed83b61f038351459b5ecaf89b8614
Napatchol Thaipanich <napatchol.tha@gmail.com>
2025-08-08 00:26:09 +0100

    chore: reorganize data folder

--- backend/api/dataset.py
+++ backend/api/dataset.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Query
 from backend.core.dataset_service import get_full_dataset
 from typing import List, Optional
-
+from backend.utils.constants import UNLABELED_PATH
 router = APIRouter()
 
 @router.get("/dataset", summary="Get dataset features")
@@ -15,4 +15,9 @@ def refresh_dataset(
     addresses: Optional[List[str]] = Query(default=None)
 ):
     dataset = get_full_dataset(refresh=True, addresses=addresses)
+    return dataset
+
+@router.get("/dataset-unlabeled", summary="Get dataset features")
+def get_dataset():
+    dataset = get_full_dataset(UNLABELED_PATH / 'groundtruth.csv')
     return dataset
--- backend/core/dataset_service.py
+++ backend/core/dataset_service.py
@@ -5,8 +5,8 @@ from typing import List, Optional
 from backend.utils.constants import LABELED_PATH, FEATURE_PATH
 from backend.core.feature_service import extract_base_feature_from_address
 
-def get_full_dataset(filename='groundtruth.csv', refresh=False, addresses:Optional[List[str]]=None):
-    df = pd.read_csv(LABELED_PATH / filename, index_col=0)
+def get_full_dataset(filename=LABELED_PATH / 'groundtruth.csv', refresh=False, addresses:Optional[List[str]]=None):
+    df = pd.read_csv(filename, index_col=0)
 
     if addresses:
         df = df[df.index.isin(addresses)]
